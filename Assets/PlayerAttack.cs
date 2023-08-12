abad094356f0a668a4ffdd953d9b2383c954f506
lijiafu <nguyenviethoan5555@gmail.com>
2023-08-12 16:53:21 +0800

    thay doi file playerattack

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -7,7 +7,7 @@ public class PlayerAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        string haha = "";
     }
 
     // Update is called once per frame
